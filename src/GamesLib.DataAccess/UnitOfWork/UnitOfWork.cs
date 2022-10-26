1dc9774a79615a4d1a2fd722e893a240b2203bda
Pashkevich Tania <pashkevichtania2004@gmail.com>
2022-10-26 16:45:20 +0300

    add unit of work

---
+++ src/GamesLib.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GamesLib.DataAccess.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _dbContext;
+
+    public UnitOfWork(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void BeginTransaction()
+    {
+        _dbContext.Database.BeginTransaction();
+    }
+
+    public void CommitTransaction()
+    {
+        _dbContext.Database.CommitTransaction();
+    }
+
+    public void RollbackTransaction()
+    {
+        _dbContext.Database.RollbackTransaction();
+    }
+}
--- test/GamesLib.ConsoleApp/Program.cs
+++ test/GamesLib.ConsoleApp/Program.cs
@@ -1,19 +1,34 @@
 ﻿using GamesLib.DataAccess.Context;
 using GamesLib.DataAccess.Model;
 using GamesLib.DataAccess.Repositories;
+using GamesLib.DataAccess.UnitOfWork;
 
-using (var context = new GamesLibContext())
+using var context = new GamesLibContext();
+
+var unitOfWork = new UnitOfWork(context);
+var gameRepo = new GameRepository(context);
+var devRepo = new DevRepository(context);
+var publisherRepo = new PublisherRepository(context);
+
+try
 {
-    var game1 = new Game { Title = "The Last Of Us Part I" };
-    var game2 = new Game { Title = "Dishonored" };
+    unitOfWork.BeginTransaction();
     
-    var gameRepository = new GameRepository(context);
+    var dev = new Dev { Title = "Naughty Dog" };
+    var publisher = new Publisher { Title = "Naughty Dog" };
 
-    gameRepository.Add(game1);
-    gameRepository.Add(game2);
 
-    foreach (var item in gameRepository.Get())
-    {
-        Console.WriteLine($"{item.Id} - {item.Title} ");
-    }
+    devRepo.Add(dev);
+    publisherRepo.Add(publisher);
+    
+    Console.WriteLine(dev.Id);
+    Console.WriteLine(publisher.Id);
+
+    gameRepo.Add(new Game { Title = "The Last Of Us Part I",  Description = "something", ReleaseDate = DateTime.Now, Publisher = publisher, Dev = dev });
+
+    unitOfWork.CommitTransaction();
+}
+catch
+{
+    unitOfWork.RollbackTransaction();
 }
